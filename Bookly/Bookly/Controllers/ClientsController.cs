57bbcfcb69bc8a36facac5beb7f1acc6fe06fc7f
hadihakim <hah092@usal.edu.lb>
2021-04-17 02:35:19 +0300

    Connect DB with Clients Controller make Index Action That return List of Clients from DB and create Index view that display The list of Clients

--- Bookly/Bookly/Controllers/ClientsController.cs
+++ Bookly/Bookly/Controllers/ClientsController.cs
@@ -7,8 +7,26 @@ using Bookly.Models;
 
 namespace Bookly.Controllers
 {
+
     public class ClientsController : Controller
     {
+        private ApplicationDbContext db;
+        public ClientsController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+        public ViewResult Index()
+        {
+            var clients = db.Clients.ToList();//This equal to select * from Clients
+            return View(clients);
+        }
+
+
         // GET: Clients
         public ActionResult Random()
         {
