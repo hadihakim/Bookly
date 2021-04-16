c68aa137d0c17e462aa498416c3098527a03cbc9
hadihakim <hah092@usal.edu.lb>
2021-04-16 17:43:13 +0300

    Connect Client Model to Database

---
+++ Bookly/Bookly/Migrations/202104161441489_AddClientModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Bookly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddClientModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clients",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        DateOfBirth = c.String(),
+                        Type = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Clients");
+        }
+    }
+}
