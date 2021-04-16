3e8b1b1020a3fb4fa5f4cf1df7d6c5bc877f26be
hadihakim <hah092@usal.edu.lb>
2021-04-16 17:27:01 +0300

    Add Models Book and client and their controllers. Add random view for Client model

---
+++ Bookly/Bookly/Controllers/ClientsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bookly.Models;
+
+namespace Bookly.Controllers
+{
+    public class ClientsController : Controller
+    {
+        // GET: Clients
+        public ActionResult Random()
+        {
+            var client1 = new Client { Name = "hadi"};
+            return View(client1);
+        }
+    }
+}
---
+++ Bookly/Bookly/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bookly.Models
+{
+    public class Client
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Type { get; set; }
+    }
+}
