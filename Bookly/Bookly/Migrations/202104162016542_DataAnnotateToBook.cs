c6f8166f7fe2e66720924a8aa938fdc98978957b
hadihakim <hah092@usal.edu.lb>
2021-04-16 23:20:05 +0300

    add-migration DataAnnotateToBook and alter Book Table in DB

---
+++ Bookly/Bookly/Migrations/202104162016542_DataAnnotateToBook.cs
@@ -0,0 +1,18 @@
+﻿namespace Bookly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataAnnotateToBook : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Books", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Books", "Name", c => c.String());
+        }
+    }
+}
--- Bookly/Bookly/Models/Book.cs
+++ Bookly/Bookly/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,10 @@ namespace Bookly.Models
 {
     public class Book
     {
+        [Required]
         public int ID { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public string Genre { get; set; }
         public int NumberInStock { get; set; }
