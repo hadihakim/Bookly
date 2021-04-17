6f6b013e5ff1e1311ba0cd6ee3be32aad81902e5
hadihakim <hah092@usal.edu.lb>
2021-04-17 04:15:41 +0300

    Add Details and Index Actions In Books Controller and View pages for each Action

--- Bookly/Bookly/Controllers/BooksController.cs
+++ Bookly/Bookly/Controllers/BooksController.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bookly.Models;
 
 namespace Bookly.Controllers
 {
+    
     public class BooksController : Controller
     {
-        // GET: Books
-        public ActionResult Index()
+        private ApplicationDbContext db;
+
+        public BooksController()
         {
-            return View();
+            db = new ApplicationDbContext();
         }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+
+        // GET: Books
+        public ViewResult Index()
+        {
+            var books = db.Books.ToList();//This equal to select * from Books
+            return View(books);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var book = db.Books.SingleOrDefault(c => c.ID == id);
+            if (book == null)
+                return HttpNotFound();
+            return View(book);
+        }
+        
+       
     }
 }
