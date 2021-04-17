7a447315b153cb6805f6b623652f2384db696313
hadihakim <hah092@usal.edu.lb>
2021-04-17 04:44:18 +0300

    Add Index Action In Reservations Controller and view page to list Reservations

---
+++ Bookly/Bookly/Controllers/ReservationsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bookly.Models;
+
+namespace Bookly.Controllers
+{
+    public class ReservationsController : Controller
+    {
+
+
+        private ApplicationDbContext db;
+        public ReservationsController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+        // GET: Reservations
+        public ViewResult Index()
+        {
+            var reservations = db.Reservations.ToList();//This equal to select * from Clients
+            return View(reservations);
+        }
+       
+       
+    }
+}
