19da1d9206515b1bdad3d6c67d3c3f8c1c04c48c
hadihakim <hah092@usal.edu.lb>
2021-04-17 05:43:56 +0300

    Delete Reservation

--- Bookly/Bookly/Controllers/ReservationsController.cs
+++ Bookly/Bookly/Controllers/ReservationsController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Bookly.Models;
+using System.Data.Entity;
 
 namespace Bookly.Controllers
 {
@@ -24,10 +26,19 @@ namespace Bookly.Controllers
         // GET: Reservations
         public ViewResult Index()
         {
-            var reservations = db.Reservations.ToList();//This equal to select * from Clients
+            var reservations = db.Reservations.Include(c => c.Book).ToList();
+            //This equal to select * from Clients
             return View(reservations);
         }
-       
-       
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            Reservation empresa = await db.Reservations.FindAsync(id);
+            db.Reservations.Remove(empresa);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
